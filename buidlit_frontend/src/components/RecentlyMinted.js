3a8c69988c48598cfe997a2a518034670f20d76d
Swarnab Garang <swarnabgarang@gmail.com>
2021-11-27 23:36:20 +0530

    removed unused imports

--- buidlit_frontend/src/components/RecentlyMinted.js
+++ buidlit_frontend/src/components/RecentlyMinted.js
@@ -1,15 +1,8 @@
-import { Button } from "@chakra-ui/button";
-import { useColorModeValue } from "@chakra-ui/color-mode";
 import { Image } from "@chakra-ui/image";
 import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/layout";
-import { useMediaQuery } from "@chakra-ui/media-query";
 import React from "react";
 
-import "./RecentlyMinted.css";
-
 const RecentlyMinted = ({ recent_nfts }) => {
-    const [isLargerThanMobile] = useMediaQuery("(min-width: 540px)");
-
     return (
         <Box>
             <Text fontSize="3xl" fontWeight={500} my={10}>
@@ -19,36 +12,6 @@ const RecentlyMinted = ({ recent_nfts }) => {
                 {recent_nfts &&
                     recent_nfts.map((nft) => {
                         return (
-                            // <div className="recentcard" key={nft._id}>
-                            //     <div className="recentcard__content">
-                            //         <div className="recentcard__front">
-                            //             <Image
-                            //                 // h={"auto"}
-                            //                 w={"70%"}
-                            //                 ml={10}
-                            //                 src={
-                            //                     "https://harmoonies.one/images/MintButtonBackground.png"
-                            //                 }
-                            //             />
-                            //         </div>
-
-                            //         <div className="recentcard__back">
-                            //             <p className="recentcard__body">
-                            //                 {nft.name}
-                            //             </p>
-                            //             <p className="recentcard__body">
-                            //                 {nft.token}
-                            //             </p>
-                            //             <p className="recentcard__body">
-                            //                 {nft.rarity}
-                            //             </p>
-                            //             <p className="recentcard__body">
-                            //                 {nft.age}
-                            //             </p>
-                            //         </div>
-                            //     </div>
-                            // </div>
-
                             <Flex
                                 key={nft._id}
                                 my={5}
